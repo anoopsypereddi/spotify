428b4d34dc3299cf7edf0f7cf5ae830493fb07b0
asypereddi <asypereddi@gmail.com>
2020-10-15 00:15:48 -0400

    WORK PLS

---
+++ flask-server/static/react/precache-manifest.a3532db9b37a658f7e4803f37f7f77a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f743fcf7be4c8d0e3860f188351be60f",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "10ffc8e7705515c9d7f4",
+    "url": "/static/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19b6b76109a74c8dcd49",
+    "url": "/static/react/static/js/2.c6f7cc46.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/react/static/js/2.c6f7cc46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ffc8e7705515c9d7f4",
+    "url": "/static/react/static/js/main.f833af0c.chunk.js"
+  },
+  {
+    "revision": "00b90c6979e7b9785281",
+    "url": "/static/react/static/js/runtime-main.3a44c33a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
